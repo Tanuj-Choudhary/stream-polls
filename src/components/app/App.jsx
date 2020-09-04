b82aae469eb63696eb3c9af334bb6334f3f51c18
Tanuj <tanujchoudhary001@gmail.com>
2020-09-04 22:09:33 +0530

    Added global styling and configured app component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Routes from '../routes/Routes';
+import './app.css';
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
