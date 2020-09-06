e70ff438d40e712d6e6f259848f4595fe6b55d60
Tanuj <tanujchoudhary001@gmail.com>
2020-09-06 19:19:23 +0530

    Added login route

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
 import Home from '../home/Home';
+import Login from '../login/Login';
+import Page404 from '../error/Page404';
 
 function Routes() {
   return (
@@ -8,6 +11,8 @@ function Routes() {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/page404" exact component={Page404} />
         </Switch>
       </BrowserRouter>
     </>
