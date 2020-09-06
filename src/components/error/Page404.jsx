6a8354daa331d204707d4eee17a11941deb8dc53
Tanuj <tanujchoudhary001@gmail.com>
2020-09-06 22:19:42 +0530

    Created 404 page

---
+++ src/components/error/Page404.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './page404.css';
+
+function Page404() {
+  return (
+    <div className="page-404">
+      <h1>Page not found.</h1>
+      <h2>Please check the URL</h2>
+    </div>
+  );
+}
+export default Page404;
