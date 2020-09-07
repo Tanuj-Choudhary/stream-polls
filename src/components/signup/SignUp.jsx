615a0fbd0acedcd20f7f8a6b0cdb6e5553b9353a
Tanuj <tanujchoudhary001@gmail.com>
2020-09-07 13:27:26 +0530

    refactored signup page and component

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,19 +1,25 @@
+// Third Party Imports
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
+// Project Imports
 import SignUpView from './SignUpView';
 import StreamPollsAPI from '../../api/StreamPollsAPI';
 
 function Login(props) {
+  // Fields state
   const initialFields = { email: '', password: '', passwordConfirm: '' };
   const [fields, setfields] = useState(initialFields);
 
+  // Errors State
   const initialErrors = {};
   const [errors, setErrors] = useState(initialErrors);
 
+  // Form valid state
   const initialFormValid = false;
   const [formValid, setformValid] = useState(initialFormValid);
 
+  // Validation handling
   function handleValidation(fields) {
     let errors = {};
     let formIsValid = true;
@@ -37,37 +43,52 @@ function Login(props) {
     setformValid(formIsValid);
   }
 
-  const onInputChange = (name, value) => {
+  // Input handle
+  const onInputChange = (event) => {
+    // Get input id as name, and value
+    const { id: name, value } = event.target;
+
+    // Create fields object with new value
     let newFields = { ...fields, [name]: value };
+
+    // Set state to new value
     setfields(newFields);
+
+    // Handle validation using new value
     handleValidation(newFields);
   };
 
+  // Form submission
   const onSubmit = async (event) => {
+    // Prevent default event (Refresh)
     event.preventDefault();
 
+    // Form not valid return alert
     if (!formValid) {
       return alert('Form is not valid');
     }
 
+    // Send data to API
     const res = await StreamPollsAPI.post(
       `http://127.0.0.1:8000/users/signup`,
       fields
     );
-    console.log(res.data);
   };
 
   function render() {
+    // Get id param from URL
     const id = props.match.params.id;
-    console.log(id);
 
+    // If not creator or user Redirect to 404
     if (id !== 'creator' && id !== 'user') {
       return <Redirect to={{ pathname: '/page404' }} />;
     }
 
     // setfields({ ...fields, role: id });
+    // Assign role according to id
     fields.role = id;
 
+    // Return Component
     return (
       <SignUpView
         fields={fields}
