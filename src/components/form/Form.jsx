139f3b3310543cfcc714fa9eec014634063cfd44
Tanuj <tanujchoudhary001@gmail.com>
2020-09-11 20:02:18 +0530

    Refactored form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,37 +1,16 @@
 // Third Party imports
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 // Styles
 import './form.css';
 
-function Form({
-  heading,
-  footerText,
-  footerLinkAddress,
-  footerLinkText,
-  onSubmit,
-  children,
-}) {
+function Form({ heading, children }) {
   return (
     <div className="form-container">
       <form className="sign-in-form">
         <h1>{heading}</h1>
 
         {children}
-
-        <div className="form-btn">
-          <button onClick={onSubmit} className="btn action-btn">
-            Sign in
-          </button>
-        </div>
-
-        <div className="footer">
-          <span className="text-2">{footerText}</span>
-          <Link className="text-2 bb-1" to={footerLinkAddress}>
-            {footerLinkText}
-          </Link>
-        </div>
       </form>
     </div>
   );
