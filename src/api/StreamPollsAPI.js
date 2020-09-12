8afc9256c420b65c6c80d9bbf2f95ecfdee435c9
Tanuj <tanujchoudhary001@gmail.com>
2020-09-12 08:51:52 +0530

    Changed api url

--- src/api/StreamPollsAPI.js
+++ src/api/StreamPollsAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Axios Configuration for stream polls API
 const axiosConfig = {
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: 'https://stream-polls.herokuapp.com/api/v1',
 };
 
 export default axios.create(axiosConfig);
